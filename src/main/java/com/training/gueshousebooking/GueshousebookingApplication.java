c04de3513201b23bdf52384d71a29f1e3f10c366
karthik <karthik.velishala54@gmail.com>
2024-05-20 22:15:50 +0530

    Commit to check merge

--- src/main/java/com/training/gueshousebooking/GueshousebookingApplication.java
+++ src/main/java/com/training/gueshousebooking/GueshousebookingApplication.java
@@ -3,6 +3,7 @@ package com.training.gueshousebooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//initial code
 @SpringBootApplication
 public class GueshousebookingApplication {
 
